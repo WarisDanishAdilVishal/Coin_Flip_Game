aebcf5b3528e0c35928e2e4878cf2e2757ab9311
MD WARIS AKHTER <waris.akhter99@gmail.com>
2025-03-15 18:36:13 +0530

    comit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  //apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://coin-flip-game-85jm.onrender.com'
 //   DB_URL: 'jdbc:postgresql://localhost:5432/coingame',
 //   DB_USERNAME: 'postgres',
 //   DB_PASSWORD: '1234',
